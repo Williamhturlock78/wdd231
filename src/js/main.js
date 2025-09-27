4486a6cb63f5e82c4d0585cf5c8ec82333bc3b5a
Williamhturlock78 <williamh78@byui.edu>
2025-09-27 13:47:21 -0600

    added js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+function setHeaderInfo(data) {
+  // insert data into disclaimer section
+  const disclaimer = document.querySelector(".disclaimer > a");
+  disclaimer.href = data.url;
+  disclaimer.innerHTML = data.fullName;
+  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+  document.querySelector("head > title").textContent = data.fullName;
+  // set the banner image
+  document.querySelector(".hero-banner > img").src = data.images[0].url;
+  // use the template function above to set the rest of the park specific info in the header
+  document.querySelector(".hero-banner__content").innerHTML =
+    parkInfoTemplate(data);
+}
+
+const parkInfoLinks = [
+
+]
+
+function mediaCardTemplate(info) {
+    return `
+    <div class= "media-card">
+    `;
+}
+
+function setParkInfo() {
+    
+}
