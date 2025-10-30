4be5bd540257ff4b20ec5f71256c72bd9f57b0b8
Williamhturlock78 <williamh78@byui.edu>
2025-10-30 10:50:54 -0600

    animation practice

---
+++ src/js/conditions.js
@@ -0,0 +1,7 @@
+import "../css/style.css";
+import "../css/conditions.css";
+
+import { getParkData, getAlerts, getVisitorCenterData } from "./parkService.mjs";
+import { setHeaderFooter } from "./main.js";
+
+const parkCode = "yell"; // Yellowstone
